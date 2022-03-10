0f9b648be353f29bb8fe3a94d57fa3760d1390d3
USER <paul881125@yahoo.com>
2022-03-10 16:18:10 +0800

    first one

---
+++ src/main/java/Ex1.java
@@ -0,0 +1,94 @@
+public class Ex1 {
+    public static void main(String[] args) {
+
+    }
+
+}
+/*
+//元的面積
+double r = 1.26;
+System.out.println(Math.Pi*Math.pow(r, r));
+
+*/
+
+/*
+int a =1;
+double b = 2;
+b=3.7;
+b=6;
+
+a = (int)1.2;
+//java不可福點數需轉型
+*/
+
+
+//short code
+/*
+ double a= 1,b= 2;
+ double max = a>b?a:b;
+ System.out.println("max = "+max);
+ */
+
+
+
+/////if else if二選一
+
+
+/*
+int a= 1,b=2;
+int max;
+if(a>b) {
+	max = a;
+}else {
+	max = b;
+}
+System.out.println("Max = "+max);
+*/
+
+
+
+
+
+		/*
+		double bmi = 21;
+		String status;
+		if(bmi < 18.5) {
+			status = "體輕";
+		}else {
+			if(bmi < 24) {
+				status = "正常";
+			}else {
+				status = "體重";
+			}
+				System.out.println("status = "+status);
+
+		}
+	  }
+	*/
+
+
+
+
+		/*		String Fname = "A123456789";
+		String Sex ;
+		if (Fname.charAt(1) == '1') {
+			Sex = "Boy";
+		}else {
+			Sex = "Girl";
+		}
+		System.out.println("Sex = "+Sex);
+		*/
+
+
+	/*	int score=85;
+		String result;
+		if(score>=60) {
+			result = "pass";
+		}else {
+			result = "fail";
+		}
+		System.out.println("result = "+result);
+		// TODO Auto-generated method stub
+*/
+
+
